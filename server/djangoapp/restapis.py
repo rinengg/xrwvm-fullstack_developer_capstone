e648d08250a49b8bce33fc04e940a79854524961
rinengg <rishika.nanda09@gmail.com>
2026-03-23 05:12:32 +0000

    fix: load .env from correct path in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,7 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
